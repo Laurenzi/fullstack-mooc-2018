f7eddcd818b49b864ba6d02c93ceb244a3c71d2e
Laurenzi <laurirutanen@gmail.com>
2018-12-04 18:28:41 +0200

    Part 4 exercise 4.12 done

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -2,9 +2,28 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog
-    .find({})
-  return response.json(blogs)
+  try {
+    const blogs = await Blog
+      .find({})
+    return response.json(blogs)
+  } catch(exception) {
+    console.log(exception)
+    response.status(400).send({ error: 'unknown error' })
+  }
+})
+
+blogsRouter.get('/:id', async (request, response) => {
+  try {
+    const blog = await Blog.findOne({ _id: request.params.id })
+    if (blog) {
+      return response.status(200).json(blog)
+    } else {
+      return response.status(404).end()
+    }
+  } catch (exception) {
+    console.log(exception)
+    response.status(400).send({ error: 'malformed id' })
+  }
 })
   
 blogsRouter.post('/', async (request, response) => {
--- part4/test/blog_api.test.js
+++ part4/test/blog_api.test.js
@@ -2,119 +2,100 @@ const supertest = require('supertest')
 const { app, server } = require('../index')
 const api = supertest(app)
 const Blog = require('../models/blog')
+const helper = require('../test/test_helper')
 
-const initialBlogs = [
-  {
-    _id: '5a422a851b54a676234d17f7',
-    title: 'React patterns',
-    author: 'Michael Chan',
-    url: 'https://reactpatterns.com/',
-    likes: 7,
-    __v: 0
-  },
-  {
-    _id: '5a422aa71b54a676234d17f8',
-    title: 'Go To Statement Considered Harmful',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-    likes: 5,
-    __v: 0
-  },
-  {
-    _id: '5a422b3a1b54a676234d17f9',
-    title: 'Canonical string reduction',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-    likes: 12,
-    __v: 0
-  },
-  {
-    _id: '5a422b891b54a676234d17fa',
-    title: 'First class tests',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
-    likes: 10,
-    __v: 0
-  },
-  {
-    _id: '5a422ba71b54a676234d17fb',
-    title: 'TDD harms architecture',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
-    likes: 0,
-    __v: 0
-  },
-  {
-    _id: '5a422bc61b54a676234d17fc',
-    title: 'Type wars',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
-    likes: 2,
-    __v: 0
-  }
-]
+describe('When there are initially some blogs saved', async () => {
 
-beforeAll(async () => {
-  await Blog.remove({})
+  beforeAll(async () => {
+    await Blog.remove({})
+  
+    for (let blog of helper.initialBlogs) {
+      let blogObject = new Blog(blog)
+      await blogObject.save()
+    }
+  })
 
-  for (let blog of initialBlogs) {
-    let blogObject = new Blog(blog)
-    await blogObject.save()
-  }
+  test('blogs are returned as json by GET /api/blogs', async () => {
+    const response = await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    expect(response.body).toEqual(helper.initialBlogs)
+  })
+
+  test('single blog can be returned as json by GET /api/blogs/:id', async () => {
+    const blogsInDatabase = await helper.blogsInDb()
+    const aBlogId = blogsInDatabase[0].id
+    
+    const response = await api
+      .get(`/api/blogs/${aBlogId}`)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    expect(response.body).toEqual(helper.initialBlogs[0])
+  })
+
+  test('404 returned by GET /api/blogs/:id with nonexisting valid id', async () => {
+    const validNonexistingId = await helper.nonExistingId()
+
+    const response = await api
+      .get(`/api/blogs/${validNonexistingId}`)
+      .expect(404)
+  })
+
+  test('400 is returned by GET /api/blogs/:id with invalid id', async () => {
+    const invalidId = "5a3d5da59070081a82a3445"
+
+    const response = await api
+      .get(`/api/blogs/${invalidId}`)
+      .expect(400)
+  })
+  
 })
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-})
+describe('Addition of a new blog', async () => {
 
-test('blogs are returned', async () => {
-  const response = await api
-    .get('/api/blogs')
+  test('a blog created without likes will have 0 likes', async () => {
+    const blogObject = {
+      title: 'Paranoidi Optimisti2',
+      author: 'Risto Siilasmaa',
+      url: 'http://ristosiilasmaa.com',
+    }
+    const response = await api
+      .post('/api/blogs').send(blogObject)
+  
+    const expected = await Blog.findOne({_id: response.body._id})
+  
+    expect(response.status).toBe(201)
+    expect(response.body).toMatchObject({title: 'Paranoidi Optimisti2', author: 'Risto Siilasmaa', url: 'http://ristosiilasmaa.com', likes: 0})
+    expect(expected).toMatchObject({title: 'Paranoidi Optimisti2', author: 'Risto Siilasmaa', likes: 0, url: 'http://ristosiilasmaa.com'})
+  })
 
-  expect(response.body).toEqual(initialBlogs)
-})
+  test('a blog can be added via post', async () => {
+    const blogObject = {
+      title: 'Paranoidi Optimisti1',
+      author: 'Risto Siilasmaa',
+      url: 'http://ristosiilasmaa.com',
+      likes: 1000,
+    }
+    const response = await api
+      .post('/api/blogs').send(blogObject)
+    expect(response.body).toMatchObject(blogObject)
+  })
 
-test('a blog can be added via post', async () => {
-  const blogObject = {
-    title: 'Paranoidi Optimisti1',
-    author: 'Risto Siilasmaa',
-    url: 'http://ristosiilasmaa.com',
-    likes: 1000,
-  }
-  const response = await api
-    .post('/api/blogs').send(blogObject)
-  expect(response.body).toMatchObject(blogObject)
-})
+  test('trying to create a blog without url or title returns with status 400', async () => {
+    const blogObject = {
+      author: 'Risto Reipas',
+      likes: 1919
+    }
 
-test('a blog created without likes will have 0 likes', async () => {
-  const blogObject = {
-    title: 'Paranoidi Optimisti2',
-    author: 'Risto Siilasmaa',
-    url: 'http://ristosiilasmaa.com',
-  }
-  const response = await api
-    .post('/api/blogs').send(blogObject)
+    const response = await api
+      .post('/api/blogs/').send(blogObject)
 
-  const expected = await Blog.findOne({_id: response.body._id})
+    expect(response.status).toBe(400)
 
-  expect(response.status).toBe(201)
-  expect(response.body).toMatchObject({title: 'Paranoidi Optimisti2', author: 'Risto Siilasmaa', url: 'http://ristosiilasmaa.com', likes: 0})
-  expect(expected).toMatchObject({title: 'Paranoidi Optimisti2', author: 'Risto Siilasmaa', likes: 0, url: 'http://ristosiilasmaa.com'})
-})
-
-test('trying to create a blog without url or title returns with status 400', async () => {
-  const blogObject = {
-    author: 'Risto Reipas',
-    likes: 1919
-  }
-
-  const response = await api
-    .post('/api/blogs/').send(blogObject)
-
-  expect(response.status).toBe(400)
+  })
 
 })
 
